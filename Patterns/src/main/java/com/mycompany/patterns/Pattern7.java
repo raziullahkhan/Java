1004341f7fee30d6d258522dcd8726c67a908498
Raziullah <raziullahkhan2222@gmail.com>
2021-09-22 10:53:45 +0530

    Added pattern problem 7

---
+++ Patterns/src/main/java/com/mycompany/patterns/Pattern7.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.patterns;
+
+/**
+ *
+ * @author raziu
+ */
+public class Pattern7 {
+    public static void main(String[] args) {
+         int n=5;
+    for(int i=n;i>=1;i--){
+    for(int j=1;j<=i;j++){
+        System.out.print(j+" ");
+    }
+            System.out.println("");
+}
+    }
+}
