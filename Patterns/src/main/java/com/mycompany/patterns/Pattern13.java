3eaab7bba52bb0e88c5d5f6235abaa9095b7035e
Raziullah <raziullahkhan2222@gmail.com>
2021-09-22 18:45:28 +0530

    Added pattern problem 13

---
+++ Patterns/src/main/java/com/mycompany/patterns/Pattern13.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.patterns;
+
+/**
+ *
+ * @author raziu
+ */
+public class Pattern13 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print("  ");
+            }
+             for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
